d3d1eab2815c3fbf113c7a10054778e881abe081
ITjiuzhanggui <zhangxinhuicf@163.com>
2019-08-28 14:54:36 +0800

    update '.'

---
+++ TEST.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+    # for name in dirs:
+    #     print(os.path.join(root, name))
